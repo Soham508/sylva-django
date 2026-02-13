3114aac58d62e43cc54c627ee0902cba03a657ba
Soham <sohamghige508@gmail.com>
2026-02-13 13:08:07 +0530

    build failure update

--- backend/sylva/firebase.py
+++ backend/sylva/firebase.py
@@ -4,13 +4,14 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 firebase_config = {
     "type": "service_account",
-    "project_id": "sylva-687f1",
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
     "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": "102471103956928836899",
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
